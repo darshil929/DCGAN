ca4ea8b11f5615ee5db39c907ca5d6c6591bdda2
darshil929 <darshilt929@gmail.com>
2024-07-05 18:41:57 +0530

    generator

--- model.py
+++ model.py
@@ -34,3 +34,36 @@ class Discriminator(nn.Module):
         
     def forward(self, x):
         return self.disc(x)
+    
+class Generator(nn.Module):
+    def __init__(self, z_dim, channel_img, features_g):
+        super(Generator, self).__init__()
+        self.net = nn.Sequential(
+            # Input: N x z_dim x 1 x 1
+            self._block(z_dim, features_g*16, 4, 1, 0), # N x features_g*16 x 4 x 4
+            self._block(features_g*16, features_g*8, 4, 2, 1), # 8x8
+            self._block(features_g*8, features_g*4, 4, 2, 1), # 16x16
+            self._block(features_g*4, features_g*2, 4, 2, 1), # 32x32
+            nn.ConvTranspose2d(
+                features_g*2, channel_img, kernel_size=4, stride=2, padding=1
+            ),
+            nn.Tanh() # [-1, 1]
+        )
+        
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            # works in opposite way of Conv2d
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+    
+    def forward(self, x):
+        return self.gen(x)
