691955a9db06cec0db4fa8ae595309a4da065574
darshil929 <darshilt929@gmail.com>
2024-07-05 18:47:43 +0530

    test function

--- model.py
+++ model.py
@@ -38,7 +38,7 @@ class Discriminator(nn.Module):
 class Generator(nn.Module):
     def __init__(self, z_dim, channel_img, features_g):
         super(Generator, self).__init__()
-        self.net = nn.Sequential(
+        self.gen = nn.Sequential(
             # Input: N x z_dim x 1 x 1
             self._block(z_dim, features_g*16, 4, 1, 0), # N x features_g*16 x 4 x 4
             self._block(features_g*16, features_g*8, 4, 2, 1), # 8x8
@@ -67,3 +67,28 @@ class Generator(nn.Module):
     
     def forward(self, x):
         return self.gen(x)
+    
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+            
+def test():
+    N, in_channels, H, W = 8, 3, 64, 64
+    z_dim = 100
+    
+    x = torch.randn((N, in_channels, H, W))
+    disc = Discriminator(in_channels, 8)
+    initialize_weights(disc)
+    assert disc(x).shape == (N, 1, 1, 1)
+    print("Discriminator test passed")
+    
+    gen = Generator(z_dim, in_channels, 8)
+    initialize_weights(gen)
+    z = torch.randn((N, z_dim, 1, 1))
+    assert gen(z).shape == (N, in_channels, H, W)
+    print("Generator test passed")
+    
+    print("Success!")
+    
+# test()
